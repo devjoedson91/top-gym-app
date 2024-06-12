8a0278a4f22303e5e79d774697e61e84ce0d6d00
Joedson Ferreira <95484503+devjoedson91@users.noreply.github.com>
2024-06-12 10:24:46 -0300

    chore - config next pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('next-pwa').WithPWA} */
+
+export const withPWA = {
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+};
+
 const nextConfig = {
   images: {
     domains: ["img.freepik.com", "tecnofit-site.s3.sa-east-1.amazonaws.com"],
